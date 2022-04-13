6ab4e34cdd6a2ca50f804d503c5137bdc374d385
konradbycz <konradbycz@gmail.com>
2022-04-13 21:20:14 +0200

    Added Socket time handler

--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -1,14 +1,27 @@
 import { Request } from "express";
 import { JwtPayload } from "jsonwebtoken";
+import { Server, Socket } from "socket.io";
+
+export type BreakData = {
+    startsIn: number;
+    duration: number;
+}
+
+export type TimeData = {
+    timeLeft: number;
+    breaks: BreakData[];
+}
 
 export interface ServerToClientEvents {
     // noArg: () => void;
     // basicEmit: (a: number, b: string, c: Buffer) => void;
     // withAck: (d: string, callback: (e: number) => void) => void;
+    sendTimeData: (data:TimeData) => void;
 }
   
 export interface ClientToServerEvents {
-    hello: string;
+    hello: () => string;
+    getTime: () => Promise<void|null>; 
 }
 
 export interface InterServerEvents {
