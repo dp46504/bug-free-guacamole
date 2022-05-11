ab19bea95f54d4933bf90fa33c99e41d77eba549
Pawel Durczak <kissonion24@gmail.com>
2022-05-11 23:34:09 +0200

    Update variables.js

--- frontend/src/variables.js
+++ frontend/src/variables.js
@@ -3,7 +3,10 @@ const main = "http://localhost:5000/api";
 const endpoints = {
   login: `${main}/login`,
   register: `${main}/register`,
-  start_workday: `${main}/start-workday`,
+  start_workday: `${main}/user/start-workday`,
+  day_info: `${main}/user/day-info`,
+  users_worktimes: `${main}/admin/users-worktimes`, //Worktime all uzytkownikow
+  user_worktimes: `${main}/admin/user-worktimes`, //all worktimes for one person
 };
 
 module.exports = {
