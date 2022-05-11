6b1cb018e25e410f80e3a471788d217983cdcbf9
Pawel Durczak <kissonion24@gmail.com>
2022-05-11 21:54:42 +0200

    Using DRY in case of gettin calendar info in user and admin dashboard

--- frontend/src/components/UserDashboard.js
+++ frontend/src/components/UserDashboard.js
@@ -16,66 +16,16 @@ import {
 import { ReactComponent as MainLogo } from "../img/main-logo.svg";
 import { ReactComponent as Circles } from "../img/circle.svg";
 import Roles from "../helpers/Roles";
+import getCalendarInfo from "../helpers/getCalendarInfo";
 
 export default function UserDashboard(props) {
   let history = useNavigate();
+  let [dayData, setDayData] = useState([]);
 
   useEffect(() => {
-    let dayArr = [];
-    let daysInMonth = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth() + 1,
-      0
-    ).getDate();
-
-    let daysInPreviousMonth = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      0
-    ).getDate();
-
-    let daysOfThePreviousMonthToAdd = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      0
-    ).getDay();
-
-    let daysOfTheNextMonthToAdd =
-      7 -
-      new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDay();
-
-    // Adding previous month days to array
-    for (let i = daysOfThePreviousMonthToAdd; i > 0; i--) {
-      dayArr.push({
-        number: daysInPreviousMonth - i + 1,
-        month: -1,
-        monthNumber: new Date().getMonth() - 1,
-        year: new Date().getFullYear(),
-      });
-    }
-    // Adding actual month days to array
-    for (let i = 1; i <= daysInMonth; i++) {
-      dayArr.push({
-        number: i,
-        month: 0,
-        monthNumber: new Date().getMonth(),
-        year: new Date().getFullYear(),
-      });
-    }
-    // Adding next month days to array
-    for (let i = 1; i <= daysOfTheNextMonthToAdd; i++) {
-      dayArr.push({
-        number: i,
-        month: 1,
-        monthNumber: new Date().getMonth() + 1,
-        year: new Date().getFullYear(),
-      });
-    }
-
-    setDayData(dayArr);
+    getCalendarInfo(setDayData);
   }, []);
 
-  let [dayData, setDayData] = useState([]);
   const monthNames = [
     "January",
     "February",
